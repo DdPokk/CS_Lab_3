6c8a81fa316374a177d13960b409d2c0adc5436d
DdPokk <9111322054@mail.ru>
2022-01-31 11:50:22 +0300

    31.01

---
+++ WPF_Lab_9_DataBindingDemo/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Lab_9_DataBindingDemo
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+}
---
+++ WPF_Lab_9_DataBindingDemo/StudentList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace WPF_Lab_9_DataBindingDemo
+{
+    public class StudentList : ObservableCollection<Student>
+    {
+    }
+}
